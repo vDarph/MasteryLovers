43ebea74b6f2a6745817fea2f1c75b70650015fc
vDarph <dario.salle@gmail.com>
2018-06-06 04:48:31 +0200

    Revert "Assets, Css"
    This reverts commit 507659e208e8e3832aa3c4caebb11a784472b5ef.
    
    # Conflicts:
    #	src/App.css
    #	src/App.js

--- src/Result.js
+++ src/Result.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import './App.css';
 import ChampionCard from './ChampionCard'
+import SummonerInfo from './SummonerInfo'
 
-class Result extends Component {
+class Result extends Component{
 
     state = {
         masteryData: {
@@ -10,38 +11,38 @@ class Result extends Component {
         }
     }
 
-    async componentWillMount() {
-
-        let endPoint =
-            "https://euw1.api.riotgames.com/lol/champion-mastery/v3/champion-masteries/by-summoner/"
-            + this.props.summonerData.id
-            + "?api_key="
+    async componentWillMount(){
+        
+        let endPoint = 
+            "https://euw1.api.riotgames.com/lol/champion-mastery/v3/champion-masteries/by-summoner/" 
+            + this.props.summonerData.id 
+            + "?api_key=" 
             + this.props.keyCode;
         let fetched = await fetch(endPoint);
         let masteryData = await fetched.json();
 
-        console.log("this are mastery data", masteryData)
-
-        endPoint =
-            "https://euw1.api.riotgames.com/lol/static-data/v3/champions?locale=en_US&champListData=image&dataById=true&api_key="
+        endPoint = 
+            "https://euw1.api.riotgames.com/lol/static-data/v3/champions?locale=en_US&champListData=image&dataById=true&api_key=" 
             + this.props.keyCode;
         fetched = await fetch(endPoint);
         let championData = await fetched.json();
-        console.log("this are champion data", championData)
+        console.log(championData)
         this.setState({
             championData,
             masteryData
         })
-    }
+      }
 
+    
+    render(){
 
-    render() {
         let ChampionCardJSx = []
         let personalMasteryData = this.state.masteryData
-        for (let i = 0; i < personalMasteryData.length; i++) {
+        console.log(personalMasteryData)
+        for(let i = 0; i < personalMasteryData.length; i++){
             let championId = personalMasteryData[i].championId;
             ChampionCardJSx.push(
-                <ChampionCard
+                <ChampionCard 
                     championId={championId}
                     masteryData={personalMasteryData[i]}
                     championData={this.state.championData.data[championId]}
@@ -49,12 +50,13 @@ class Result extends Component {
             )
         }
 
-        if (this.props.keyCode == null) {
+        if (this.props.keyCode == null){
             this.contenutoJSX = null
         }
 
-        return (
+        return(
             <div>
+                <SummonerInfo />
                 {ChampionCardJSx}
             </div>
         )
---
+++ src/SummonerInfo.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class SummonerInfo extends Component{
+
+    render(){
+        return(
+            <div className="summonerInfo">
+
+            </div>
+        )
+    }
+}
+
+export default SummonerInfo
+
+
+//http://ddragon.leagueoflegends.com/cdn/6.24.1/img/profileicon/588.png
